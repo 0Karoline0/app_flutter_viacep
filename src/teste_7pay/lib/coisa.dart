8a0791dc73e38d00da9d977b250292f9f757f36a
0Karoline0 <116676166+0Karoline0@users.noreply.github.com>
2023-08-16 20:57:17 -0300

    Andamento-QuadroResultado
    - Limitar área
    - Criar footer

--- src/teste_7pay/lib/coisa.dart
+++ src/teste_7pay/lib/coisa.dart
@@ -12,142 +12,42 @@ class Coisa extends StatefulWidget {
 }
 
 class _CoisaState extends State<Coisa> {
+  double ibge = 100, bairro = 100, cep = 100;
+  double logradouro = 150, complemento = 150;
+  double localidade = 80, opcoes = 80, uf = 80;
+
   @override
   Widget build(BuildContext context) {
-
-    double cardHeight = MediaQuery.of(context).size.height*0.4;
-    double cardWidth = MediaQuery.of(context).size.width*0.8;
-
-    double ibge = 100;
-    double bairro = 100;
-    double cep = 100;
-    double logradouro = 150;
-    double complemento = 200;
-    double localidade, opcoes = 80;
-    double uf = 50;
-
+    double cardHeight = MediaQuery.of(context).size.height * 0.4;
+    double cardWidth = MediaQuery.of(context).size.width * 0.8;
 
     return Scaffold(
       body: Container(
           padding: EdgeInsets.all(40),
           child: Stack(
             children: [
-              Container(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 10, top: 12, bottom: 12, right: 10),
-                    child: Column(children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            alignment: Alignment.center,
-                            width: cep,
-                            color: Colors.amber,
-                            child: const Text(
-                              'CEP',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            width: logradouro,
-                            color: Colors.amber,
-                            child: const Text(
-                              'Logradouro',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            width: complemento,
-                            color: Colors.amber,
-                            child: const Text(
-                              'Complemento',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            width: bairro,
-                            color: Colors.amber,
-                            child: const Text(
-                              'Bairro',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            width: localidade,
-                            color: Colors.amber,
-                            child: const Text(
-                              'Localidade',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            width: uf,
-                            color: Colors.amber,
-                            child: const Text(
-                              'UF',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            width: ibge,
-                            color: Colors.amber,
-                            child: const Text(
-                              'IBGE',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 30,
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            width: opcoes,
-                            color: Colors.amber,
-                            child: const Text(
-                              'Opções',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10,),
-                      Row(
-                        children: [
-                          Container(
-                            width: cep,
-                              alignment: Alignment.center,
-                              child: Text('testesssssss')
-                          ),
-                        ],
-                      )
-                    ]),
-                  ),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 10, top: 12, bottom: 12, right: 10),
+                  child: Column(children: [
+                    //============HEADER
+                    header(),
+                    //============RESULTADO
+                    resultado('12345-220', 'Rua 2', 'n°12', 'Setor Oeste',
+                        'GOIÂNIA', 'GO', '12321321', 'teste'),
+                    resultado('12345-220', 'Rua 2', 'n°12', 'Setor Oeste',
+                        'GOIÂNIA', 'GO', '12321321', 'teste'),
+                    resultado('12345-220', 'Rua 2', 'n°12', 'Setor Oeste',
+                        'GOIÂNIA', 'GO', '12321321', 'teste'),
+                    resultado('12345-220', 'Rua 2', 'n°12', 'Setor Oeste',
+                        'GOIÂNIA', 'GO', '12321321', 'teste'),
+                    resultado('12345-220', 'Rua 2', 'n°12', 'Setor Oeste',
+                        'GOIÂNIA', 'GO', '12321321', 'teste'),
+                    resultado('12345-220', 'Rua 2', 'n°12', 'Setor Oeste',
+                        'GOIÂNIA', 'GO', '12321321', 'teste'),
+                    //resultado(),
+                  ]),
                 ),
               ),
               Container(
@@ -159,4 +59,98 @@ class _CoisaState extends State<Coisa> {
           )),
     );
   }
+
+  Widget header() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            celulas('CEP', cep, true),
+            celulas('Logradouro', logradouro, true),
+            celulas('Complemento', complemento, true),
+            celulas('Bairro', bairro, true),
+            celulas('Localidade', localidade, true),
+            celulas('UF', uf, true),
+            celulas('IBGE', ibge, true),
+            celulas('Opções', opcoes, true),
+          ],
+        ),
+        const SizedBox(
+          height: 15,
+        )
+      ],
+    );
+  }
+
+  Widget resultado(
+      String resultCEP,
+      String resultLog,
+      String resultComp,
+      String resultBairro,
+      String resultLocal,
+      String resultUF,
+      String resultIBGE,
+      String resultOpcoes) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 9,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            celulas('$resultCEP', cep, false),
+            celulas('$resultLog', logradouro, false),
+            celulas('$resultComp', complemento, false),
+            celulas('$resultBairro', bairro, false),
+            celulas('$resultLocal', localidade, false),
+            celulas('$resultUF', uf, false),
+            celulas('$resultIBGE', ibge, false),
+            Row(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  width: opcoes,
+                  child: const Icon(Icons.list),
+                ),
+                const SizedBox(
+                  width: 30,
+                ),
+              ],
+            ),
+          ],
+        ),
+        Divider(),
+      ],
+    );
+  }
+}
+
+Widget celulas(String informacao, double largura, bool bold) {
+  Widget negrito;
+
+  if (bold == true) {
+    negrito = Text(
+      informacao,
+      style: const TextStyle(fontWeight: FontWeight.bold),
+    );
+  } else {
+    negrito = Text(
+      informacao,
+    );
+  }
+
+  return Row(
+    children: [
+      Container(
+        alignment: Alignment.center,
+        width: largura,
+        child: negrito,
+      ),
+      const SizedBox(
+        width: 30,
+      ),
+    ],
+  );
 }
